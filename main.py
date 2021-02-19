08e115648aa174466263e9c5220a0b9ad3c2abe7
Keyboard Destroyer <43577127+Alexey0109@users.noreply.github.com>
2021-02-19 20:46:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,21 @@ with open('stats.json', 'r') as f:
 
 if 'balance' not in data:
     data['balance'] = {}
+
 if 'stats' not in data:
     data['stats'] = {}
+
+if 'positive' not in data['stats']:
+    data['stats']['positive'] = 0
+
+if 'negative' not in data['stats']:
+    data['stats']['negative'] = 0
+
+if 'n_coins' not in data['stats']:
+    data['stats']['n_coins'] = 0
+
+if 'p_coins' not in data['stats']:
+    data['stats']['p_coins'] = 0
 
 with open('stats.json', 'w') as f:
     json.dump(data, f, indent=4)
@@ -29,26 +42,41 @@ def callback_inline(call):
             data["count"] = str(int(data["count"]) - 1)
             keys = list(data.keys())
             mode = int(data.pop(keys[1]))
+            if keys[1] == str(call.message.chat.id):
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Ваш предыдущий ход еще не завершен!")
+                #return
             with open('stats.json', 'r') as f:                                
                 stats = json.load(f)
             if call.data == "coin_y":
+                stats["stats"]["positive"] = str(int(stats["stats"]["positive"]) + 1)                    
+                stats['stats'][str(call.message.chat.id)]['positive'] = str(int(stats['stats'][str(call.message.chat.id)]['positive']) + 1)
                 bot.send_message(keys[1], 'Противник положил монетку!')
                 stats['balance'][str(keys[1])] = str(int(stats['balance'][str(keys[1])]) + 3)
                 if mode == 0:
+                    stats['stats']['n_coins'] = str(int(stats['stats']['n_coins']) + 3)
                     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Противник не положил монетку!")
                     stats['balance'][str(call.message.chat.id)] = str(int(stats['balance'][str(call.message.chat.id)]) - 1) 
                 else:
+                    stats['stats']['p_coins'] = str(int(stats['stats']['p_coins']) + 4)
                     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Противник положил монетку!")
                     stats['balance'][str(call.message.chat.id)] = str(int(stats['balance'][str(call.message.chat.id)]) + 2) 
             if call.data == "coin_n":
+                stats["stats"]["negative"] = str(int(stats["stats"]["negative"]) + 1)
+                stats['stats'][str(call.message.chat.id)]['negative'] = str(int(stats['stats'][str(call.message.chat.id)]['negative']) + 1)
                 bot.send_message(keys[1], 'Противник не положил монетку!')
                 if mode == 1:
+                    stats['stats']['n_coins'] = str(int(stats['stats']['n_coins']) + 3)
                     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Противник положил монетку!")
                     stats['balance'][str(call.message.chat.id)] = str(int(stats['balance'][str(call.message.chat.id)]) + 3)
                 else:
                     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Противник не положил монетку!")
+            
+            bot.send_message(call.message.chat.id, f'Ваш баланс: {stats["balance"][str(call.message.chat.id)]}')
+            bot.send_message(keys[1], f'Ваш баланс: {stats["balance"][str(keys[1])]}')
+            
             with open('stats.json', 'w') as f:
                 json.dump(stats, f, indent=4)
+        
         else:
             bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Ожидание хода")
             data["count"] = str(int(data["count"]) + 1)
@@ -56,20 +84,36 @@ def callback_inline(call):
             if call.data == "coin_y":
                 with open('stats.json', 'r') as f:
                     stats = json.load(f)
+                stats["stats"]["positive"] = str(int(stats["stats"]["positive"]) + 1)
+                stats['stats'][str(call.message.chat.id)]['positive'] = str(int(stats['stats'][str(call.message.chat.id)]['positive']) + 1)
+                
                 mode = 1
                 stats['balance'][str(call.message.chat.id)] = str(int(stats['balance'][str(call.message.chat.id)]) - 1)
                 with open('stats.json', 'w') as f:
                     json.dump(stats, f, indent=4)
             data[str(call.message.chat.id)] = str(mode)
+            if call.data == "coin_n":
+                with open('stats.json', 'r') as f:
+                    stats = json.load(f)
+                stats["stats"]["negative"] = str(int(stats["stats"]["negative"]) + 1)
+                stats['stats'][str(call.message.chat.id)]['negative'] = str(int(stats['stats'][str(call.message.chat.id)]['negative']) + 1)
+                
+                with open('stats.json', 'w') as f:
+                    json.dump(stats, f, indent=4)
+
         with open('queue.json', 'w') as f:
             json.dump(data, f, indent=4)
 
 @bot.message_handler(commands=['start', 'info'])
 def display_info(message):
-    bot.send_message(message.chat.id, "Привет! Этот бот - математическая игра про монетку и игровой автомат! Правила довольно просты: Если вы кинете монетку то ваш противник получит от автрмата 3 монетки и наоборот - если протмвник кинет монетку то вы получите 3 монетки! Монетку можно не кидать и тогда ваш противник не получит ничего!")
+    bot.send_message(message.chat.id, f"Привет! Этот бот - математическая игра про монетку и игровой автомат! Правила довольно просты: Если вы кинете монетку то ваш противник получит от автомата 3 монетки и наоборот - если противник кинет монетку то вы получите 3 монетки! Монетку можно не кидать и тогда ваш противник не получит ничего!")
     with open('stats.json', 'r') as f:
         data = json.load(f)
     data['balance'][str(message.chat.id)] = 10
+    if str(message.chat.id) not in data['stats']:
+        data['stats'][str(message.chat.id)] = {}
+        data['stats'][str(message.chat.id)]['positive'] = 0
+        data['stats'][str(message.chat.id)]['negative'] = 0
     with open('stats.json', 'w') as f:
         json.dump(data, f, indent=4)
 
@@ -86,5 +130,21 @@ def coin(message):
     menu.add(types.InlineKeyboardButton(text = 'Положить монетку', callback_data ='coin_y'))
     menu.add(types.InlineKeyboardButton(text = 'Не класть монетку', callback_data ='coin_n'))
     bot.send_message(message.chat.id, text ='Игра', reply_markup = menu)
+
+@bot.message_handler(commands=['stats'])
+def getstats(message):
+    with open('stats.json', 'r') as f:
+        data = json.load(f)
+    if str(message.chat.id) not in data['stats']:
+        data['stats'][str(message.chat.id)]['positive'] = 0
+        data['stats'][str(message.chat.id)]['negative'] = 0
+    general_positive = data['stats']['positive']
+    general_negative = data['stats']['negative']
+    general_ncoins = data['stats']['n_coins']
+    general_pcoins = data['stats']['p_coins']
+    
+    player_positive = data['stats'][str(message.chat.id)]['positive']
+    player_negative = data['stats'][str(message.chat.id)]['negative']
+    bot.send_message(message.chat.id, f'Статистика:\nОбщая статистика:\nИгроки ложили монетку {general_positive} раз\nИгроки не ложили монетку {general_negative} раз\nСтатистика по монетам: {general_pcoins}/{general_ncoins}\n(Игроки дали монету/Не дали монету)\nСтатистика игрока @{message.chat.username}:\nВы ложили монетку {player_positive} раз\nВы не ложили монетку {player_negative} раз')
 
 bot.polling()
